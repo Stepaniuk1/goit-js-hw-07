31f8234a7577514212d08b19e2cedad6d95a5fd3
Stepaniuk1 <122210657+Stepaniuk1@users.noreply.github.com>
2023-05-31 23:46:39 +0300

    completed hm

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(`.gallery`);
+
+galleryItems.forEach(function (item) {
+  let imgAtt = item.original;
+
+  const listItem = document.createElement(`li`);
+  listItem.classList.add("gallery__item");
+
+  const link = document.createElement(`a`);
+  link.classList.add("gallery__link");
+  link.href = imgAtt;
+
+  const image = document.createElement(`img`);
+  image.setAttribute(`data-source`, `${imgAtt}`);
+  image.alt = item.description;
+  image.classList.add("gallery__image");
+  image.src = item.preview;
+
+  link.appendChild(image);
+  listItem.appendChild(link);
+  gallery.appendChild(listItem);
+});
+
+function galleryOpen(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") return;
+
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}"></img>;
+`);
+
+  instance.show();
+}
+
+gallery.addEventListener(`click`, galleryOpen);
 
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(`.gallery`);
+
+galleryItems.forEach(function (item) {
+  let imgAtt = item.original;
+
+  const listItem = document.createElement(`li`);
+  listItem.classList.add("gallery__item");
+
+  const link = document.createElement(`a`);
+  link.classList.add("gallery__link");
+  link.href = imgAtt;
+
+  const image = document.createElement(`img`);
+  image.classList.add("gallery__image");
+  image.src = item.preview;
+  image.setAttribute(`data-source`, `${imgAtt}`);
+  image.alt = item.description;
+
+  link.appendChild(image);
+  listItem.appendChild(link);
+  gallery.appendChild(listItem);
+});
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: `alt`,
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
